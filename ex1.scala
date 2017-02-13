e9fd3c3670325b96d7ed8af37eeab4e71729d208
Mirko Kämpf <mirko@cloudera.com>
2017-02-13 17:50:23 +0000

    some changes have been done to be able to use the project in CDSW.

--- ex1.scala
+++ ex1.scala
@@ -4,9 +4,14 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType};
 import org.graphframes._
 
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+// this is used to implicitly convert an RDD to a DataFrame.
+import sqlContext.implicits._
+  
 val customSchema = StructType(Array(StructField("src", IntegerType, true),StructField("dst", IntegerType, true)))
 
-val edges = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("delimiter", " ").schema(customSchema).load("facebook_combined.txt")
+val edges = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("delimiter", " ").schema(customSchema).load("data/facebook_combined.txt")
 
 val n1 = edges.select("src").distinct()
 val n2 = edges.select("dst").distinct()
@@ -31,5 +36,11 @@ pr2.vertices.printSchema
 
 val scatter = pr2.vertices.join(tcr, pr2.vertices.col("id").equalTo(tcr("id")))
 
-scatter.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("scatter_1.csv")
+scatter.printSchema
+  
+val newNames = Seq("name", "id", "pr", "count", "name2", "id2")
+  
+val scatter2 = scatter.toDF(newNames: _*)
+
+scatter2.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save("scatter_3.csv")
 
